c91687cf27cd287aa3c4166fe72783ec6224a15f
Palabandla Vinay <140831480+vinaypalabandla@users.noreply.github.com>
2026-03-10 16:47:52 +0530

    Add files via upload

---
+++ 04.WEEK-04/17.Day17_Adv.SQL Server_Intro_to_DotNet/HandsOnPblm_DAY17/SQLQuery1_HandsOn_prblm01_DAY17_w4.sql
@@ -0,0 +1,81 @@
+﻿/*Level-2 Problem 1: Transactions and Trigger Implementation
+Scenario
+Auto retail company wants to ensure stock consistency while placing orders. Whenever an order is placed, stock should reduce automatically and transaction should rollback if stock is insufficient.
+
+📌 Requirements 
+- Write a transaction to insert data into orders and order_items tables.
+- Check stock availability before confirming order.
+- Create a trigger to reduce stock quantity after order insertion.
+- Rollback transaction if stock quantity is insufficient.
+*/
+
+USE CompanyDB;
+
+DROP TRIGGER trg_AutoRedcueStock
+
+--Create trigger
+CREATE TRIGGER trg_AutoRedcueStock
+ON order_items
+AFTER INSERT
+AS
+BEGIN
+ 
+ IF EXISTS (
+ SELECT 1
+ FROM stocks s
+ JOIN (
+    SELECT product_id, SUM(quantity) AS totalqty
+	FROM inserted i
+	GROUP BY product_id)
+	i ON s.product_id =i.product_id
+	WHERE s.quantity<i.totalqty
+	)
+
+	BEGIN
+	RAISERROR('Insufficent stock.',16,1);
+	ROLLBACK TRANSACTION;
+	RETURN;
+	END
+
+	UPDATE s
+	SET s.quantity = s.quantity-i.totalqty
+	from stocks s
+	JOIN (
+	SELECT product_id, SUM(quantity) AS totalqty
+	FROM inserted
+	GROUP BY product_id
+	) i ON s.product_id =i.product_id
+	END;
+	--palce new orderes
+	BEGIN TRY
+	    BEGIN TRANSACTION;
+
+		INSERT INTO orders
+		VALUES (107,1,1, GETDATE(), DATEADD(DAY,5,GETDATE()),NULL,1,1);
+
+		INSERT INTO order_items VALUES
+		(107,1,1,5,50000, 0.10),
+		(107,2,2,3,20000,0.05);
+
+		COMMIT TRANSACTION;
+		PRINT 'order Succuess';
+	END TRY
+BEGIN CATCH
+		ROLLBACK TRANSACTION;
+END CATCH;
+
+
+---==============
+SELECT * FROM stocks;
+select * FROM  order_items;
+SELECT * FROM order_items WHERE order_id = 107;
+
+UPDATE stocks
+SET quantity = 40
+WHERE product_id = 2;
+
+
+
+
+
+
---
+++ 04.WEEK-04/17.Day17_Adv.SQL Server_Intro_to_DotNet/HandsOnPblm_DAY17/SQLQuery2_HandsOn_Prblm02_DAY17_w4.sql
@@ -0,0 +1,50 @@
+﻿-------------------PRBLM 02---
+/*📌 Requirements 
+- Begin a transaction when cancelling an order.
+- Restore stock quantities based on order_items.
+- Update order_status to 3.
+- Use SAVEPOINT before stock restoration.
+- If stock restoration fails, rollback to SAVEPOINT.
+- Commit transaction only if all operations succeed.
+*/
+
+USE CompanyDB;
+--- create transction 
+BEGIN TRY
+    BEGIN TRANSACTION;
+
+    --Create Savepoint HERE
+    SAVE TRANSACTION SavePoint;
+    --Restore Stock
+    UPDATE s
+    SET s.quantity = s.quantity + oi.quantity
+    FROM stocks s
+    JOIN order_items oi 
+        ON s.product_id = oi.product_id
+    WHERE oi.order_id = 106;  
+
+    -- Update Order Status to Rejected 2
+    UPDATE orders
+    SET order_status =5
+    WHERE order_id = 106;
+
+    COMMIT TRANSACTION;
+    PRINT 'Order Cancelled Successfully';
+
+END TRY
+BEGIN CATCH
+    -- Rollback to Savepoint if error occurs
+    ROLLBACK TRANSACTION SavePoint;
+    -- Rollback full transaction
+    ROLLBACK TRANSACTION;
+
+    PRINT 'Cancellation Failed';
+
+END CATCH;
+
+----checking==
+SELECT order_id, order_status
+FROM orders
+WHERE order_id = 106;
+
+select *  from orders;
