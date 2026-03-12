7350f966d40818743cc498b1d608c3a32541a758
Palabandla Vinay <140831480+vinaypalabandla@users.noreply.github.com>
2026-03-12 18:14:09 +0530

    Add files via upload

---
+++ 04.WEEK-04/19.Day19_C#_Methods_OOPs/HandsOnSolutionDay19th_w4/P2_SimpleCalCulator.cs
@@ -0,0 +1,55 @@
+﻿/*Level-1 Problem 1: Simple Calculator Using Methods
+Scenario:
+A small retail shop wants a simple calculator application to perform addition and subtraction operations using reusable methods.
+Requirements:
+1. Create a class named Calculator.
+2. Create methods Add(int a, int b) and Subtract(int a, int b).
+3. Each method should return the result.
+4. In Main(), create an object and call the methods.
+5. Display the output.
+Technical Constraints:
+1. Use method parameters and return types properly.
+2. Use appropriate access modifiers.
+3. No global variables allowed.
+*/
+class Calculator
+{
+    public int Add(int x, int y)
+    {
+        return x + y;
+    }
+    public int Sub(int x, int y)
+    {
+        return x - y;
+    }
+    public int Mul(int x, int y)
+    {
+        return x * y;
+    }
+    public int Div(int x, int y)
+    {
+        return x / y;
+    }
+}
+// now create main class 
+class CalApp
+{
+    static void Main(string[] args)
+    {
+        //Object creation
+        Calculator cal = new Calculator();
+
+        Console.Write("Enter First Number: ");
+        int x = int.Parse(Console.ReadLine());
+        Console.Write("\nEnter Second Number: ");
+        int y = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("\n=========Result Perfomring=========");
+
+        Console.WriteLine("Addition: " + cal.Add(x, y));
+        Console.WriteLine("Substract: " + cal.Sub(x, y));
+        Console.WriteLine("Multiplication: " + cal.Mul(x, y));
+        Console.WriteLine("Division: " + cal.Div(x, y));
+
+    }
+}
---
+++ 04.WEEK-04/19.Day19_C#_Methods_OOPs/HandsOnSolutionDay19th_w4/P3_StudentGradeCal.cs
@@ -0,0 +1,78 @@
+﻿/*Level-1 Problem 2: Student Grade Calculator
+Scenario:
+A school wants to calculate the average marks of a student using a class-based approach.
+Requirements:
+1. Create a class Student.
+2. Create method CalculateAverage(int m1, int m2, int m3).
+3. Return the average marks.
+4. Display grade based on average.
+Technical Constraints:
+1. Use return type double for average.
+2. Avoid hard-coded values.
+Expectations:
+Clear separation of logic inside methods.
+Learning Outcome:
+Learn method creation, return values, and basic OOP concepts.
+Sample Input: 
+80 70 90
+Sample Output: 
+Average = 80, Grade = A
+*/
+
+//Create  a class
+using System.Diagnostics;
+
+class Student
+{
+    public double CalculateAvg(int m1, int m2, int m3)
+    {
+        return (m1 + m2 + m3) / 3;
+    }
+    public char GetGrade(double avg)
+    {
+        if (avg >= 80)
+        {
+            return 'A';
+        }
+        else if (avg >= 60)
+        {
+            return 'B';
+        }
+        else if (avg >= 40)
+        {
+            return 'C';
+        }
+        else
+        {
+            return 'F';
+        }
+
+    }
+    // main class
+    class StudentApp
+    {
+        static void Main(string[] args)
+        {
+            //object creation
+            Student sa = new Student();
+            Console.WriteLine("\n==========**PLEASE ENTER THE MARKS**=================");
+            Console.Write("Enter Marks m1: ");
+            int m1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter Marks m2: ");
+            int m2 = int.Parse(Console.ReadLine());
+            Console.Write("Enter Marks m3: ");
+            int m3 = int.Parse(Console.ReadLine());
+
+            double avg = sa.CalculateAvg(m1, m2, m3);
+            Console.WriteLine("\n==========AVERAGE*SCORE=================");
+            Console.WriteLine(avg);
+            Console.WriteLine("\n==========GRADE=================");
+            char grade = sa.GetGrade(avg);
+            Console.WriteLine(grade);
+
+            Console.WriteLine("\n=============AVERAGE MARKS WITH GRADE================");
+            Console.WriteLine("Average = " + avg + ",Grade = " + grade);
+        }
+    }
+}
+
