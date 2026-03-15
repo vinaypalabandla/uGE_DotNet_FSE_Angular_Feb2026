2938ca307d89b262e63d07a869842c954c90916e
Palabandla Vinay <140831480+vinaypalabandla@users.noreply.github.com>
2026-03-15 16:43:23 +0530

    Add files via upload

---
+++ 04.WEEK-04/20.Day20_C#_OOPs/CaseStudies_HandsOn_C#Eg_And_SQL_Day20/bankcaseStudy/BankAccount.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class BankAccount
+    {
+        private decimal _balance;
+        private string _accountNumber;
+        private string _accountHolder;
+
+        public decimal Balance
+        {
+            get
+            {
+                return _balance;
+            }
+        }
+
+        public string AccountNumber
+        {
+            get
+            {
+                return _accountNumber;
+            }
+        }
+
+        public string AccountHolder
+        {
+            get { return _accountHolder; }
+            set
+            {
+
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Account Holder Name cannot be empty");
+                }
+
+                _accountHolder = value;
+            }
+        }
+
+
+        public BankAccount(string accountNumber, string accountHolder, decimal balance = 0)
+        {
+
+            if (string.IsNullOrEmpty(accountNumber))
+            {
+                throw new ArgumentNullException("Account Number cannot be empty");
+            }
+
+            if (string.IsNullOrEmpty(accountHolder))
+            {
+                throw new ArgumentNullException("Account Holder Name cannot be empty");
+            }
+
+            if (balance < 0)
+            {
+                throw new ArgumentOutOfRangeException("Balance cannot be nagative");
+            }
+
+
+            _accountNumber = accountNumber;
+            _accountHolder = accountHolder;
+            _balance = balance;
+        }
+
+
+
+        public void Deposit(decimal amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("Amount cannot be nagative");
+            }
+
+            _balance += amount;
+
+            Console.WriteLine($"Amount successfully added to balance. Updated Balance : INR.{_balance.ToString("F2")}");
+            string time = DateTime.Now.ToString("dd/MM HH:mm");
+            Console.WriteLine($"Alert: INR {amount.ToString("N2")} credited to a/c {_accountNumber} on {time}. Clear Balance: INR {_balance.ToString("N2")}.");
+
+        }
+
+
+        public bool Withdraw(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Amount cannot be zero or lessthan zero");
+                return false;
+            }
+
+            if (amount > _balance)
+            {
+                Console.WriteLine("Insufficient Balance");
+                return false;
+            }
+
+            _balance -= amount;
+
+            Console.WriteLine($"Withdrawn successfully.  Updated Balance. INR.{_balance.ToString("F2")}");
+            string time = DateTime.Now.ToString("dd/MMM HH:mm");
+
+            Console.WriteLine($"Alert: INR {amount.ToString("N2")} debited from a/c {_accountNumber} on {time}. Clear Balance: INR {_balance.ToString("N2")}.NOT you please aprroach bank");
+
+
+            return true;
+
+        }
+
+    }
+}
---
+++ 04.WEEK-04/20.Day20_C#_OOPs/CaseStudies_HandsOn_C#Eg_And_SQL_Day20/bankcaseStudy/Program.cs
@@ -0,0 +1,24 @@
+﻿using ConsoleApp8;
+
+namespace ConsoleApp39
+{ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var acc = new BankAccount("LT987654", "Jonas", 1200m);
+
+            acc.Deposit(300);              // OK
+
+            bool success = acc.Withdraw(2000);  // false (insufficient funds)
+            acc.Withdraw(800);             // true
+
+            Console.WriteLine(acc.Balance);     // 700
+
+            acc.AccountHolder = "Jonas Jonaitis";  // OK (with validation)
+
+
+            Console.ReadLine();
+        }
+    }
+}
