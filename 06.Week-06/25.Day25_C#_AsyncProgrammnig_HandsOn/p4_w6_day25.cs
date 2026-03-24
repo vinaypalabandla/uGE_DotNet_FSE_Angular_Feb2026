6982923da04e1caa9185944a5d935662db41d7e4
Palabandla Vinay <140831480+vinaypalabandla@users.noreply.github.com>
2026-03-24 08:25:04 +0530

    Add files via upload

---
+++ 06.Week-06/25.Day25_C#_AsyncProgrammnig_HandsOn/p4_w6_day25.cs
@@ -0,0 +1,43 @@
+﻿//using System;
+//using System.Threading.Tasks;
+
+//class Program
+//{
+//    static async Task Main(string[] args)
+//    {
+//        Console.WriteLine("Order Processing Started...");
+
+//        await ProcessOrderAsync();
+
+//        Console.WriteLine("Order Processing Completed");
+//    }
+
+//    static async Task ProcessOrderAsync()
+//    {
+//        await VerifyPaymentAsync();
+//        await CheckInventoryAsync();
+//        await ConfirmOrderAsync();
+//    }
+
+//    static async Task VerifyPaymentAsync()
+//    {
+//        Console.WriteLine("Verifying Payment...");
+//        await Task.Delay(2000);
+//        Console.WriteLine("Payment Verified \n");
+//    }
+
+
+//    static async Task CheckInventoryAsync()
+//    {
+//        Console.WriteLine("Checking Inventory...");
+//        Console.WriteLine("Inventory Available \n");
+//    }
+
+
+//    static async Task ConfirmOrderAsync()
+//    {
+//        Console.WriteLine("Confirming Order...");
+//        await Task.Delay(1000);
+//        Console.WriteLine("Order Confirmed \n");
+//    }
+//}
