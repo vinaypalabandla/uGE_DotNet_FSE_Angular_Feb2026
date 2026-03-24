89751929f0fc6fd3389c13c4bb3ef330078c53c8
Palabandla Vinay <140831480+vinaypalabandla@users.noreply.github.com>
2026-03-24 08:31:49 +0530

    Add files via upload

---
+++ 06.Week-06/25.Day25_C#_AsyncProgrammnig_HandsOn/p5_w6_day25.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+
+        TextWriterTraceListener listener =
+            new TextWriterTraceListener("trace_log.txt");
+
+        Trace.Listeners.Add(listener);
+        Trace.AutoFlush = true;
+
+        Trace.WriteLine("Order Processing Started...");
+
+        try
+        {
+            ValidateOrder();
+            ProcessPayment();
+            UpdateInventory();
+            GenerateInvoice();
+
+            Trace.TraceInformation("Order processed successfully!");
+        }
+        catch (Exception ex)
+        {
+            Trace.WriteLine("ERROR: " + ex.Message);
+        }
+
+        Trace.WriteLine("Order Processing Ended.");
+
+        Console.WriteLine("Process completed. Check trace_log.txt file.");
+
+        Trace.Close();
+    }
+
+
+    static void ValidateOrder()
+    {
+        Trace.WriteLine("Step 1: Validating Order...");
+    }
+
+
+    static void ProcessPayment()
+    {
+        Trace.WriteLine("Step 2: Processing Payment...");
+
+        throw new Exception("Payment Failed!");
+    }
+
+
+    static void UpdateInventory()
+    {
+        Trace.WriteLine("Step 3: Updating Inventory...");
+    }
+
+    static void GenerateInvoice()
+    {
+        Trace.WriteLine("Step 4: Generating Invoice...");
+    }
+}
