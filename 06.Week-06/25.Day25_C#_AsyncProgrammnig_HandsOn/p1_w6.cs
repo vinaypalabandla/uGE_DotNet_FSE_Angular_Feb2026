1bc64a8a975dad67e5566b1519b94ac22da81429
Palabandla Vinay <140831480+vinaypalabandla@users.noreply.github.com>
2026-03-24 08:27:02 +0530

    Restore async logging example in p1_w6.cs

--- 06.Week-06/25.Day25_C#_AsyncProgrammnig_HandsOn/p1_w6.cs
+++ 06.Week-06/25.Day25_C#_AsyncProgrammnig_HandsOn/p1_w6.cs
@@ -1,33 +1,33 @@
-﻿//using System;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
-//class Program
-//{
-//    //async method
-//    public static async Task WriteLogAsync(string message)
-//    {
-//        Console.WriteLine("Writing log: " + message);
+class Program
+{
+   //async method
+   public static async Task WriteLogAsync(string message)
+   {
+       Console.WriteLine("Writing log: " + message);
 
-//        // file delay 2 sec
-//        await Task.Delay(2000);
+       // file delay 2 sec
+       await Task.Delay(2000);
 
-//        Console.WriteLine("Log written: " + message);
-//    }
+       Console.WriteLine("Log written: " + message);
+   }
 
-//    static async Task Main(string[] args)
-//    {
-//        Console.WriteLine("Application Started...\n");
+   static async Task Main(string[] args)
+   {
+       Console.WriteLine("Application Started...\n");
 
 
-//        Task t1 = WriteLogAsync("User logged in");
-//        Task t2 = WriteLogAsync("Data loaded");
-//        Task t3 = WriteLogAsync("File uploaded");
+       Task t1 = WriteLogAsync("User logged in");
+       Task t2 = WriteLogAsync("Data loaded");
+       Task t3 = WriteLogAsync("File uploaded");
 
-//        Console.WriteLine("\nMain thread is free to do other work...\n");
+       Console.WriteLine("\nMain thread is free to do other work...\n");
 
-//        // Wait for all tasks to complete
-//        await Task.WhenAll(t1, t2, t3);
+       // Wait for all tasks to complete
+       await Task.WhenAll(t1, t2, t3);
 
-//        Console.WriteLine("\nAll logs completed!");
-//    }
-//}
+       Console.WriteLine("\nAll logs completed!");
+   }
+}
