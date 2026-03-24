9c1b0033968e5be2433f859de70905be43531c3e
Palabandla Vinay <140831480+vinaypalabandla@users.noreply.github.com>
2026-03-24 08:26:36 +0530

    Uncommented the C# program for product pricing

--- 06.Week-06/25.Day25_C#_AsyncProgrammnig_HandsOn/p2_w6.cs
+++ 06.Week-06/25.Day25_C#_AsyncProgrammnig_HandsOn/p2_w6.cs
@@ -1,25 +1,25 @@
-﻿//using System;
+﻿using System;
 
-//class Program
-//{
-//    static void Main(string[] args)
-//    {
-//        Console.Write("Enter Product Name: ");
-//        string productName = Console.ReadLine();
+class Program
+{
+   static void Main(string[] args)
+   {
+       Console.Write("Enter Product Name: ");
+       string productName = Console.ReadLine();
 
-//        Console.Write("Enter Product Price: ");
-//        double price = double.Parse(Console.ReadLine());
+       Console.Write("Enter Product Price: ");
+       double price = double.Parse(Console.ReadLine());
 
-//        Console.Write("Enter Discount Percentage: ");
-//        double discount = double.Parse(Console.ReadLine());
-
-
-//        double finalPrice = price - (price * discount / 100);
+       Console.Write("Enter Discount Percentage: ");
+       double discount = double.Parse(Console.ReadLine());
 
 
-//        Console.WriteLine("\nProduct: " + productName);
-//        Console.WriteLine("Original Price: " + price);
-//        Console.WriteLine("Discount: " + discount + "%");
-//        Console.WriteLine("Final Price: " + finalPrice);
-//    }
-//}
+       double finalPrice = price - (price * discount / 100);
+
+
+       Console.WriteLine("\nProduct: " + productName);
+       Console.WriteLine("Original Price: " + price);
+       Console.WriteLine("Discount: " + discount + "%");
+       Console.WriteLine("Final Price: " + finalPrice);
+   }
+}
