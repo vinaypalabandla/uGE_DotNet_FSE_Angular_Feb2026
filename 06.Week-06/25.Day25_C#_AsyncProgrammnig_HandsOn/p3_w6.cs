a7f148feaf805bdfba202dd3d5fe60c4788e9b59
Palabandla Vinay <140831480+vinaypalabandla@users.noreply.github.com>
2026-03-24 08:22:27 +0530

    Add files via upload

---
+++ 06.Week-06/25.Day25_C#_AsyncProgrammnig_HandsOn/p3_w6.cs
@@ -0,0 +1,43 @@
+﻿//using System;
+
+
+//class Program
+//{
+
+//    static void GenerateSalesReport()
+//    {
+//        Console.WriteLine("Sales Report Started...");
+//        Thread.Sleep(3000);
+//        Console.WriteLine("Sales Report Completed!");
+//    }
+
+
+//    static void GenerateInventoryReport()
+//    {
+//        Console.WriteLine("Inventory Report Started...");
+//        Thread.Sleep(2000);
+//        Console.WriteLine("Inventory Report Completed!");
+//    }
+
+
+//    static void GenerateCustomerReport()
+//    {
+//        Console.WriteLine("Customer Report Started...");
+//        Thread.Sleep(4000);
+//        Console.WriteLine("Customer Report Completed!");
+//    }
+
+//    static void Main(string[] args)
+//    {
+//        Console.WriteLine("Starting Reports...");
+
+//        Task t1 = Task.Run(() => GenerateSalesReport());
+//        Task t2 = Task.Run(() => GenerateInventoryReport());
+//        Task t3 = Task.Run(() => GenerateCustomerReport());
+
+
+//        Task.WaitAll(t1, t2, t3);
+
+//        Console.WriteLine("\nAll Reports Completed!");
+//    }
+//}
