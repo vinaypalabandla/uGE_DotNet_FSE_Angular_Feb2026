f213f979ebd210e3435f1d5b85443fcc92a058a1
Palabandla Vinay <140831480+vinaypalabandla@users.noreply.github.com>
2026-03-26 22:58:31 +0530

    Add files via upload

---
+++ 06.Week-06/26.Day26_ADO.NET_HandsOn_prblms/P1_Day26_w6_SRPHandsOn.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SRPHandsOnExample
+{
+        //create Student Class
+     class Student
+    {
+       //auto properties
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public int Marks { get; set; }
+
+    }
+    //create StudentRespository class
+    class StudentRespository
+    {
+        //create Student Object store in list using private it acces inside class
+       private List<Student> stu = new List<Student>();
+        public void AddStudent(Student student)
+        {
+            stu.Add(student);//adds student object in list
+        }
+        public List<Student> GetAllStudents() // method return retrun all stuudnets
+        {
+            return stu;// return list
+        }
+    }
+    // create class for RepostGenerator
+    class ReportGenerator
+    {
+        public void GenarateReport(List<Student> stu)
+        {
+            Console.WriteLine("==========Student Report======");
+            foreach (var student in stu)
+            {
+                Console.WriteLine("ID: " + student.StudentId);
+                Console.WriteLine("Name: " + student.StudentName);
+                Console.WriteLine("Marks: " + student.Marks);
+            }
+        } 
+    }
+
+
+    //Main Program
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating object for StudentRepository class
+            StudentRespository repo = new StudentRespository();
+            // create object for ReportGenerator class
+            ReportGenerator report =  new ReportGenerator();
+
+            repo.AddStudent(new Student { StudentId = 1, StudentName = "vinay", Marks = 84 });
+            repo.AddStudent(new Student { StudentId = 2, StudentName = "vinayKumar", Marks = 75});
+
+            var students = repo.GetAllStudents();
+            //Sends data to report generator to print
+            report.GenarateReport(students);
+
+            Console.ReadLine();
+
+        }
+    }
+
+}
---
+++ 06.Week-06/26.Day26_ADO.NET_HandsOn_prblms/P2_Day26_w6_OCPHandsOn.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace OCPExample
+{
+    // 1. Interface
+    interface IDiscountStrategy
+    {
+        double CalculateDiscount(double amount);
+    }
+
+    // 2. Regular Customer
+    class RegularCustomerDiscount : IDiscountStrategy
+    {
+        public double CalculateDiscount(double amount)
+        {
+            return amount * 0.10;
+        }
+    }
+
+    // Premium Customer
+    class PremiumCustomerDiscount : IDiscountStrategy
+    {
+        public double CalculateDiscount(double amount)
+        {
+            return amount * 0.20;
+        }
+    }
+
+    // VIP Customer
+    class VipCustomerDiscount : IDiscountStrategy
+    {
+        public double CalculateDiscount(double amount)
+        {
+            return amount * 0.30;
+        }
+    }
+
+    // 3. Final Price Calculator
+    class DiscountCalculator
+    {
+        public double GetFinalPrice(double amount, IDiscountStrategy discountStrategy)
+        {
+            double discount = discountStrategy.CalculateDiscount(amount);
+            return amount - discount;
+        }
+    }
+
+    // Main Program
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DiscountCalculator calculator = new DiscountCalculator();
+
+            double amount = 1000;
+
+            IDiscountStrategy regular = new RegularCustomerDiscount();
+            IDiscountStrategy premium = new PremiumCustomerDiscount();
+            IDiscountStrategy vip = new VipCustomerDiscount();
+
+            Console.WriteLine("Regular Final Price: " + calculator.GetFinalPrice(amount, regular));
+            Console.WriteLine("Premium Final Price: " + calculator.GetFinalPrice(amount, premium));
+            Console.WriteLine("VIP Final Price: " + calculator.GetFinalPrice(amount, vip));
+
+            Console.ReadLine();
+        }
+    }
+}
