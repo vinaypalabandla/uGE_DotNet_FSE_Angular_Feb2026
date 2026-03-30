dbe7a7405931e3fb93e588df9df0dffe1580eb24
Palabandla Vinay <140831480+vinaypalabandla@users.noreply.github.com>
2026-03-30 22:23:34 +0530

    Add files via upload

---
+++ 07.Week-07/30.Day-30_HandsOn/Controllers/ContactController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebApplication3.Controllers
+{
+    public class ContactController : Controller
+    {
+        // Static List with sample data
+        public static List<ContactInfo> contacts = new List<ContactInfo>
+        {
+            new ContactInfo { ContactId = 1, FirstName = "Vinay", LastName = "Kumar", CompanyName = "CTS", EmailId = "vinay@gmail.com", MobileNo = 9999999999, Designation = "Developer" },
+            new ContactInfo { ContactId = 2, FirstName = "Rahul", LastName = "Kumar", CompanyName = "TCS", EmailId = "rahul@gmail.com", MobileNo = 8888888888, Designation = "Tester" }
+        };
+
+        // Show all contacts
+        public IActionResult ShowContacts()
+        {
+            return View(contacts);
+        }
+
+        // Get contact by ID
+        public IActionResult GetContactById(int id)
+        {
+            ContactInfo contact = contacts.FirstOrDefault(c => c.ContactId == id);
+            return View(contact);
+        }
+
+        // GET Add Contact
+        [HttpGet]
+        public IActionResult AddContact()
+        {
+            return View();
+        }
+
+        // POSTAdd Contact
+        [HttpPost]
+        public IActionResult AddContact(ContactInfo contactInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                contacts.Add(contactInfo);
+                return RedirectToAction("ShowContacts");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Invalid data. Some validations failed.";
+                return View();
+            }
+        }
+    }
+}
---
+++ 07.Week-07/30.Day-30_HandsOn/Models/ContactInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication3.Models
+{
+    public class ContactInfo
+    {
+          [Required(ErrorMessage = "Contact Id is required")]
+            public int ContactId { get; set; }
+
+            [Required(ErrorMessage = "First Name is required")]
+            [StringLength(10, MinimumLength = 3, ErrorMessage = "First Name must be 3 to 10 characters")]
+            public string FirstName { get; set; }
+
+            [Required(ErrorMessage = "Last Name is required")]
+            [StringLength(10, MinimumLength = 3, ErrorMessage = "Last Name must be 3 to 10 characters")]
+            public string LastName { get; set; }
+
+            [Required(ErrorMessage = "Company Name is required")]
+            [StringLength(10, MinimumLength = 3, ErrorMessage = "Company Name must be 3 to 10 characters")]
+            public string CompanyName { get; set; }
+
+            [Required(ErrorMessage = "Email is required")]
+            [EmailAddress(ErrorMessage = "Invalid Email format")]
+            public string EmailId { get; set; }
+
+            [Required(ErrorMessage = "Mobile Number is required")]
+            [Range(1000000000, 9999999999, ErrorMessage = "Enter valid 10-digit number")]
+            public long MobileNo { get; set; }
+
+            [Required(ErrorMessage = "Designation is required")]
+            [StringLength(10, MinimumLength = 2, ErrorMessage = "Designation must be 2 to 10 characters")]
+            public string Designation { get; set; }
+        }
+    }
