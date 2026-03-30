5186fee4af10fb141076ceba0f9a5f51f4d1d76b
Palabandla Vinay <140831480+vinaypalabandla@users.noreply.github.com>
2026-03-30 22:24:43 +0530

    Add files via upload

---
+++ 07.Week-07/30.Day-30_HandsOn/Program.cs
@@ -0,0 +1,36 @@
+namespace WebApplication3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapStaticAssets();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Contact}/{action=ShowContacts}/{id?}")
+                .WithStaticAssets();
+
+            app.Run();
+        }
+    }
+}
