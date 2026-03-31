b1d42a5c7b32e9151269b3114cc31171984b11a9
Palabandla Vinay <140831480+vinaypalabandla@users.noreply.github.com>
2026-03-31 23:22:26 +0530

    Add files via upload

---
+++ 07.Week-07/31.Day-30_HandsOn/FeedBackForm_P4/FeedbackController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        // GET: feedback form
+        [HttpGet]
+        [Route("feedback/form")]
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        // POST: handle form submission
+        [HttpPost]
+        [Route("feedback/form")]
+        public IActionResult Form(string name, string comments, int rating)
+        {
+            if (rating >= 4)
+            {
+                ViewData["Message"] = "Thank You for your feedback!";
+            }
+            else
+            {
+                ViewData["Message"] = "We will improve based on your feedback!";
+            }
+
+            return View();
+        }
+    }
+}
