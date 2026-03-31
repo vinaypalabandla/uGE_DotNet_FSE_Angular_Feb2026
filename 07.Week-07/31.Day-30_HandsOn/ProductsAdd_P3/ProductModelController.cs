9e6f5d84dcfdf2997bfec29cb0ba936ee8f39180
Palabandla Vinay <140831480+vinaypalabandla@users.noreply.github.com>
2026-03-31 23:27:13 +0530

    Add files via upload

---
+++ 07.Week-07/31.Day-30_HandsOn/ProductsAdd_P3/ProductModelController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class ProductModelController : Controller
+    {
+        private static List<dynamic> products = new List<dynamic>();
+
+        [HttpGet]
+        [Route("product/index")]
+        public IActionResult Index1()
+        {
+            ViewBag.Products = products;
+            return View();
+        }
+        [HttpPost]
+        [Route("product/index")]
+        public IActionResult Index1(string name, double price, int quantity)
+        {
+            // add product to list
+            products.Add(new
+            {
+                Name = name,
+                Price = price,
+                Quantity = quantity
+            });
+
+            ViewBag.Products = products;
+            return View();
+        }
+        [HttpGet]
+        [Route("product/add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+    }
