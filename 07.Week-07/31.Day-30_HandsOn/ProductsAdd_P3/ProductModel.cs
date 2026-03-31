42a598f477f6f96f62113f0d43de97fe77dfd8e7
Palabandla Vinay <140831480+vinaypalabandla@users.noreply.github.com>
2026-03-31 23:28:29 +0530

    Add files via upload

---
+++ 07.Week-07/31.Day-30_HandsOn/ProductsAdd_P3/ProductModel.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication2.Models
+{
+    public class ProductModel
+    {
+        
+            public string Name { get; set; }
+            public double Price { get; set; }
+            public int Quantity { get; set; }
+        
+
+    }
+}
