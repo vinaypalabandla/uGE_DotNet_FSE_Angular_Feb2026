fcb0b973860ee36c4c4395596aae348c0c0401d8
Palabandla Vinay <140831480+vinaypalabandla@users.noreply.github.com>
2026-03-31 23:24:32 +0530

    Add files via upload

---
+++ 07.Week-07/31.Day-30_HandsOn/StudentRegistration_p1/StudentController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc; 
+
+namespace WebApplication2.Controllers
+{
+    [Route("student")] // attribute route
+   public class StudentController : Controller
+    {
+        // GET 
+        [HttpGet("register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // POST
+        [HttpPost("submit")]
+        public IActionResult Submit(string studentName, int age, string course)
+        {
+            // Redirect to Display action with data
+            return RedirectToAction("Display", new
+            {
+                name = studentName,
+                age = age,
+                course = course
+            });
+        }
+
+        // GET
+        [HttpGet("display")]
+        public IActionResult Display(string name, int age, string course)
+        {
+           
+            ViewBag.Name = name;
+            ViewBag.Age = age;
+            ViewBag.Course = course;
+
+            return View();
+        }
+
+    }
+}
