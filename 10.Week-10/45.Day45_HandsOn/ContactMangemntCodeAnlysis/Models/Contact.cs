a5dbc961f83a55e38d177b0e6b6120bb9b1d6bc7
Palabandla Vinay <140831480+vinaypalabandla@users.noreply.github.com>
2026-04-22 19:54:35 +0530

    Add files via upload

---
+++ 10.Week-10/45.Day45_HandsOn/ContactMangemntCodeAnlysis/Models/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactMangemntCodeAnlysis.Models
+{
+   public class Contact
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+    }
+}
---
+++ 10.Week-10/45.Day45_HandsOn/ContactMangemntCodeAnlysis/Services/ContactService.cs
@@ -0,0 +1,73 @@
+﻿using ContactMangemntCodeAnlysis.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace ContactMangemntCodeAnlysis.Services
+{
+    public class ContactService : IContactService
+    {
+        //In Memory list to store contacts
+        private readonly List<Contact> _contacts = new();
+
+        public void AddContact(Contact contact)
+        {
+            ValidateContact(contact);
+            //Assigning the ID here
+            contact.Id = _contacts.Count + 1;
+            _contacts.Add(contact);
+        }
+
+        public void UpdateContact(int id, Contact updatedContact)
+        {
+            ValidateContact(updatedContact);
+
+            var contact = FindContactById(id);
+            if (contact == null)
+            {
+                Console.WriteLine("Contact not found.");
+                return;
+            }
+
+            contact.Name = updatedContact.Name;
+            contact.Email = updatedContact.Email;
+            contact.Phone = updatedContact.Phone;
+        }
+
+        public void DeleteContact(int id)
+        {
+            var contact = FindContactById(id);
+            if (contact == null)
+            {
+                Console.WriteLine("Contact not found.");
+                return;
+            }
+
+            _contacts.Remove(contact);
+        }
+
+        public List<Contact> GetAllContacts()
+        {
+            return _contacts;
+        }
+
+
+        private Contact? FindContactById(int id)
+        {
+            return _contacts.FirstOrDefault(c => c.Id == id);
+        }
+
+        private void ValidateContact(Contact contact)
+        {
+            if (string.IsNullOrWhiteSpace(contact.Name))
+                throw new ArgumentException("Name is required");
+
+            if (string.IsNullOrWhiteSpace(contact.Email))
+                throw new ArgumentException("Email is required");
+
+            if (string.IsNullOrWhiteSpace(contact.Phone))
+                throw new ArgumentException("Phone is required");
+        }
+    }
+    }
