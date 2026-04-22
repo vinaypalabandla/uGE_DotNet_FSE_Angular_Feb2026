c1e1788b265f8f8ce74fe557166158ddae175665
Palabandla Vinay <140831480+vinaypalabandla@users.noreply.github.com>
2026-04-22 20:07:20 +0530

    Add files via upload

---
+++ 10.Week-10/46.Day46._HandsOn/P1_HandsOn/Program.cs
@@ -0,0 +1,24 @@
+﻿using ContactMangemntCodeAnlysis.Services;
+
+namespace ContactMangemntCodeAnlysis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new ContactService();
+
+            service.AddContact(new Models.Contact
+            {
+                Name = "Vinay",
+                Email = "vinay@gmail.com",
+                Phone = "6699523322"
+            });
+            Console.WriteLine("All Contacts:");
+            foreach(var contact in service .GetAllContacts())
+            {
+                Console.WriteLine($"{{contact.Id}} - {{contact.Name}} - {{contact.Email}}\"");
+            }
+        }
+    }
+}
---
+++ 10.Week-10/46.Day46._HandsOn/P1_HandsOn/Services/IContactService.cs
@@ -0,0 +1,19 @@
+﻿using ContactMangemntCodeAnlysis.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactMangemntCodeAnlysis.Services
+{
+    public class IContactService
+    {
+        //Add new Contcat
+        void AddContact(Contact contact);
+        //Update existing contact
+        void UpdateContact(int id, Contact updatedContact);
+        //Delete contact by  id
+        void DeleteContact(int id);
+        //Get all Contacts
+        List<Contact> GetAllContacts();
+    }
+}
