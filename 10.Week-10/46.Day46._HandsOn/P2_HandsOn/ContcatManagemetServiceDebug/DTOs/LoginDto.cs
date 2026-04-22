db5c04d52bc6f6d9ec8445ad87ee4ffe73631b09
Palabandla Vinay <140831480+vinaypalabandla@users.noreply.github.com>
2026-04-22 22:02:12 +0530

    Add files via upload

---
+++ 10.Week-10/46.Day46._HandsOn/P2_HandsOn/ContcatManagemetServiceDebug/DTOs/LoginDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContcatManagemetServiceDebug.DTOs
+{
+    public class LoginDto
+    {
+        [Required]
+        public string EmailId { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ 10.Week-10/46.Day46._HandsOn/P2_HandsOn/ContcatManagemetServiceDebug/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using ContcatManagemetServiceDebug.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContcatManagemetServiceDebug.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<UserInfo> Users { get; set; }
+    }
+}
