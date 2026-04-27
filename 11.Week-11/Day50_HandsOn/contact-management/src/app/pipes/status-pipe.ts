28c649da6f9d2aed948540fdb97e2d7117023a45
Palabandla Vinay <140831480+vinaypalabandla@users.noreply.github.com>
2026-04-27 21:21:46 +0530

    Add files via upload

---
+++ 11.Week-11/Day50_HandsOn/contact-management/src/app/pipes/status-pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'status',
+  
+})
+export class StatusPipe implements PipeTransform {
+  transform(value: boolean): string {
+    return value ? 'Active' : 'Inactive';
+  }
+}
