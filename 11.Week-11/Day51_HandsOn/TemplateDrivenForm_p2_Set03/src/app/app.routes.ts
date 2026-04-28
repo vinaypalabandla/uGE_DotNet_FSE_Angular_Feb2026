014bb581ac30a22b97949cdeac4b562b0fd7b730
Palabandla Vinay <140831480+vinaypalabandla@users.noreply.github.com>
2026-04-28 23:27:08 +0530

    Delete 11.Week-11/Day51_HandsOn directory

--- 11.Week-11/Day51_HandsOn/TemplateDrivenForm_p2_Set03/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-//import { ContactDetail } from './contact-detail/contact-detail';
-import { ContactList } from './contact-list/contact-list';
-//import { AddContact } from './add-contact/add-contact';
-
-export const routes: Routes = [
- 
-];
