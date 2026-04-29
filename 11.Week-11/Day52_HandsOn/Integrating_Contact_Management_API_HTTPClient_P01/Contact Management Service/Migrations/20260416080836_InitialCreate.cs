ccdc04b32767d03408bb46e8078d07ac036cee8e
Palabandla Vinay <140831480+vinaypalabandla@users.noreply.github.com>
2026-04-29 22:47:50 +0530

    Add files via upload

---
+++ 11.Week-11/Day52_HandsOn/Integrating_Contact_Management_API_HTTPClient_P01/Contact Management Service/Migrations/20260416080836_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Contact_Management_Service.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    ContactId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.ContactId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacts");
+        }
+    }
+}
---
+++ 11.Week-11/Day52_HandsOn/Integrating_Contact_Management_API_HTTPClient_P01/Contact Management Service/Services/ContactService.cs
@@ -0,0 +1,41 @@
+﻿using Contact_Management_Service.Models;
+using Contact_Management_Service.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contact_Management_Service.Services
+{
+    public class ContactService : IContactService
+    {
+        private readonly IContactRepository _repo;
+
+        public ContactService(IContactRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<IEnumerable<Contact>> GetAll()
+        {
+            return await _repo.GetAll();
+        }
+
+        public async Task<Contact> GetById(int id)
+        {
+            return await _repo.GetById(id);
+        }
+
+        public async Task Add(Contact contact)
+        {
+            await _repo.Add(contact);
+        }
+
+        public async Task Update(Contact contact)
+        {
+            await _repo.Update(contact);
+        }
+
+        public async Task Delete(int id)
+        {
+            await _repo.Delete(id);
+        }
+    }
+}
